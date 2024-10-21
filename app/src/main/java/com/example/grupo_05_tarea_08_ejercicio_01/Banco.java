aefa4f486c1948490d7d5080db170e345877cf52
WizardCrazy <martinnicasio13@gmail.com>
2024-10-20 23:04:44 -0500

    Create class Cliente - Banco

---
+++ app/src/main/java/com/example/grupo_05_tarea_08_ejercicio_01/Banco.java
@@ -0,0 +1,12 @@
+package com.example.grupo_05_tarea_08_ejercicio_01;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Banco implements Serializable {
+    private ArrayList<Cliente> ListaCliente = new ArrayList<>();
+
+    public ArrayList<Cliente> getListaCliente() {
+        return ListaCliente;
+    }
+}
---
+++ app/src/main/java/com/example/grupo_05_tarea_08_ejercicio_01/Cliente.java
@@ -0,0 +1,7 @@
+package com.example.grupo_05_tarea_08_ejercicio_01;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+
+}
